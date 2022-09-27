9b5af098c983658dfc629818cddb56a3f650061d
Heni <henichauhan20@gmail.com>
2022-09-27 18:57:01 +0530

    wish

--- src/Card.jsx
+++ src/Card.jsx
@@ -8,13 +8,22 @@ const Card = () => (
           <div className="balloonThree" />
           <div className="balloonFour" />
         </div>
+        <p> I hope your day is filled with lots of love and
+          laughter! May all of your birthday wishes come true.</p>
       </div>
       <div className="cardInside">
         <h3 className="back">HAPPY BIRTHDAY !</h3>
-        <p>Hey ...,</p>
         <p>
-          Happy birthday!! I hope your day is filled with lots of love and
-          laughter! May all of your birthday wishes come true.
+          I hope you find someone who never makes you question your own self-worth.
+          I hope you find someone who chases your happiness as much as their own.
+          I hope you find someone who supports you in the things you are passinate about.
+          I hope you find someone whom you can laugh with and sit in silence with and share your deepest secrets with .
+          I hope you find someone is your lover, your partner , and your friend.
+          I hope you find someone who treats you as their equal,who learns and grows with you and beside you.
+          I hope you find someone who appreciates all the tiny details that make up who you are.
+          I hope you find someone who respects your heart,your family and your values.
+          I hope you find someone who never makes you question your own self-worth.
+          I hope you find someone who reminds you that you deserve the love you give...
         </p>
         <p className="name">Heniii</p>
       </div>
