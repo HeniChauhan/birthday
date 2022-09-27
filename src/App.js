cead39b9f5cf06642648035775ca5048b1e03bbe
Heni <henichauhan20@gmail.com>
2022-09-27 11:01:25 +0530

    msg

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <Card />
+      <div style={{fontSize:'20px',color:'white'}}>Hover on Card... </div>
     </div>
   );
 }
