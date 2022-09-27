bcfeb8c6c9ec9c6c0a2ec7ac5df8a9341a56ed04
Heni <henichauhan20@gmail.com>
2022-09-27 10:32:30 +0530

    2nd commit

---
+++ src/Card.jsx
@@ -0,0 +1,23 @@
+export default () => (
+    <div className="birthdayCard">
+      <div className="cardFront">
+        <h3 className="happy">HAPPY BIRTHDAY...!!</h3>
+        <div className="balloons">
+          <div className="balloonOne" />
+          <div className="balloonTwo" />
+          <div className="balloonThree" />
+          <div className="balloonFour" />
+        </div>
+      </div>
+      <div className="cardInside">
+        <h3 className="back">HAPPY BIRTHDAY !</h3>
+        <p>Hey ...,</p>
+        <p>
+          Happy birthday!! I hope your day is filled with lots of love and
+          laughter! May all of your birthday wishes come true.
+        </p>
+        <p className="name">Heniii</p>
+      </div>
+    </div>
+  );
+  
